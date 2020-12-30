83df244f90fa5ec2ff1030169b9577f61681ea2d
mostlypanda <sarthakmittal1461@gmail.com>
2020-12-30 17:54:56 +0530

    db is connected

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+require('dotenv').config();
+
 const express=require('express');
 const bodyparser=require('body-parser');
 const mongoose=require('mongoose');
 const app=express();
 
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+mongoose.Promise=global.Promise;
+mongoose.connect(process.env.MONGO_URL,{ useNewUrlParser: true }).then(()=>
+    {console.log("DB is connected");}    
+).catch((err)=>{
+    console.log(err);
+    process.exit();
+})
+
 app.get('/',(req,res)=>{
     res.send("Welcome to the class");
 });
-
+// app.get('/check',(req,res)=>{
+//     res.send("checking");
+// })
 const Port=3000;
 
 app.listen(Port,(err)=>{
     if(err) console.log(err);
-    else console.log(`port is live at ${Port}`)
+    else console.log(`port is live at ${Port}`);
 });
