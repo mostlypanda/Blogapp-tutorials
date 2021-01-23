4c50ff4acdd995db0bd4b5407d2d1aa5a7c00586
Sarthak Mittal <sarthakmittal1461@gmail.com>
2021-01-23 16:19:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,9 +23,7 @@ app.get('/',(req,res)=>{
 });
 
 require('./routes/route')(app);
-app.use('*',(req,res,next)=>{
-    res.status(404).json({"msg":"Not found"});
-});
+
 
 
 const Port=process.env.PORT||3000;
